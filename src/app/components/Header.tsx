fe5e2d0b899b426724cbc4acdc86222f276df8a1
Quiarom <userquiarom@gmail.com>
2023-10-25 16:01:10 -0400

    Initial Commit

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+    return (
+        <>
+            <header className="w-full">
+                <nav className="flex">
+                    <button>
+                            Connect Wallet
+                    </button>
+                </nav>
+            </header>
+        </>
+    )
+}
---
+++ src/app/home/page.tsx
@@ -0,0 +1,16 @@
+import Header from "../components/Header"
+
+
+export default function Home() {
+    return(
+        <>
+            <Header />
+            <main>
+                <h1>E-Commerce Web3</h1>
+            </main>
+            <footer>
+                Hecho con ❤
+            </footer>
+        </>
+    )
+}
