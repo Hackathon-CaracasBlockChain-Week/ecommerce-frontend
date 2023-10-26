0ce4d1eef8c7a0f0892fa30bac98867f51341866
quiarom <userquiarom@gmail.com>
2023-10-26 18:10:27 -0400

    List of payment plataform

--- src/app/payment/page.jsx
+++ src/app/payment/page.jsx
@@ -17,16 +17,37 @@ export default function PaymentLandingPage() {
                                 </a>
                             </li>
                         </ul>
-                        <div className="flex flex-wrap">
+                        <ul className="my-3 space-y-3">
+                            <li className="">
+                                <a href="#" className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+                                    El Dorado
+                                </a>
+                            </li>
+                        </ul>
+                        <ul className="my-3 space-y-3">
+                            <li className="">
+                                <a href="#" className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+                                    Paypal
+                                </a>
+                            </li>
+                        </ul>
+                        <ul className="my-3 space-y-3">
+                            <li className="">
+                                <a href="#" className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+                                    WalletConnect
+                                </a>
+                            </li>
+                        </ul>
+                        <div className="flex flex-wrap justify-between">
                             <p>Total a pagar:</p>
                             <p>55$</p>
                         </div>
                     </div>
-                    <aside className="max-w-md w-96 bg-[#f6f7f9] p-14">
+                    <aside className="max-w-md w-96 bg-[#D2E0FB] p-14">
                         <div className="container justify-center items-center">
                             <img className="h-36" src="https://firebasestorage.googleapis.com/v0/b/ecommerce-ccs-blockchain.appspot.com/o/product-review.png?alt=media&token=e97ba839-fcbe-4b82-8d99-9ef441adff4a" alt="" />
                             <h1>Título de Producto</h1>
-                            <p>Pedrito, el vendedor</p>
+                            <p className="mt-8 mb-24 font-body4 text-gray-dark">Pedrito, el vendedor</p>
                         </div>
                         <div>
                             <div className="mb-16"></div>
@@ -38,6 +59,9 @@ export default function PaymentLandingPage() {
                                 </span>
                             </p>
                         </div>
+                        <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Comprar
+                        </button>
                     </aside>
                 </div>
             </main>
